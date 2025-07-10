ddbbe3ee94649959fa6e12ab9893429a6a489389
Shivam Kumar <shivam108ku@gmail.com>
2025-07-10 12:36:40 +0530

    add logout endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -95,7 +95,13 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "you hit the signup end point",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({
+      message: "Logout Sucessfully",
+    });
+  } catch (error) {
+    console.error("Logout Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };

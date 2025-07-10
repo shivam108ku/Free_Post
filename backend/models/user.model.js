d054cbacdf91575ed208eb05eaee7d673f19c853
Shivam Kumar <shivam108ku@gmail.com>
2025-07-10 10:33:46 +0530

    add userSchema

---
+++ backend/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose, { mongo } from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+    username:{
+        type: String,
+        require: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 20,
+    },
+    fullName:{
+        type: String,
+        require: true,
+    },
+    password:{
+        type: String,
+        require: true,
+        minLength: 6,
+        maxLength: 20,
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    followers:[
+        {
+            type: mongoose.Schema.Types.ObjectId, //
+            ref:"User",
+            default:[] 
+        }
+    ],
+     following:[
+        {
+            type: mongoose.Schema.Types.ObjectId, //
+            ref:"User",
+            default:[] 
+        }
+    ],
+    profileImg:{
+        type: String,
+        default: "",
+    },
+     coverImg:{
+        type: String,
+        default: "",
+    },
+    bio:{
+        type: String,
+        default: "",
+    },
+    link:{
+        type: String,
+        default: "",
+    },
+
+}, {timestamps:true});
+
+const User = mongoose.model("User",userSchema);
+ 
+export default User;
+

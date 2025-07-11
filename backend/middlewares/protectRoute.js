bc604dedf6b198ded2de1d447f59dee958595f27
Shivam Kumar <shivam108ku@gmail.com>
2025-07-11 21:54:25 +0530

    fix

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -11,6 +11,7 @@ export const protectRoute = async (req , res , next)=>{
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         if(!decoded){
              return res.status(401).json({error: "Unauthorized: Invalid token"})
+             
         }
 
         const user = await User.findById(decoded.userId).select("-password");

7bc373d61d02cc8a3727f9d268dc88633297ce13
Shivam Kumar <shivam108ku@gmail.com>
2025-07-12 12:04:45 +0530

    add getSuggested USer Function

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { getUserProfile, followUnfollowUser } from '../controllers/user.controller.js';
+import { getUserProfile, getSuggestedUser , followUnfollowUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/profile/:username', protectRoute ,getUserProfile);
-
-// router.get('/suggested',getUserProfile)
- router.post('/follow/:id', protectRoute ,followUnfollowUser)
+router.get('/suggested', protectRoute, getSuggestedUser)
+router.post('/follow/:id', protectRoute ,followUnfollowUser)
 // router.post('/update', protectRoute ,updateUserProfile) 
 
 export default router;
